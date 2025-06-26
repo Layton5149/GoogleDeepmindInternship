39912c47f17ea481cc5037794807ffdf0ffac1d3
Layton Cripps <l.k.cripps@swansea.ac.uk>
2025-06-26 11:57:57 +0100

    EDA and PPG files are loaded correctly into 10 second segment arrays. these data types can have feature extraction performed on them. NOTE: if end model is unreliable, come back to commit with (ALL DATA POINTS LOADED) and try applying some preprocessing/ filtering techniques on data

--- HeartRateVariabilityOnStressClassification/main.py
+++ HeartRateVariabilityOnStressClassification/main.py
@@ -48,13 +48,12 @@ start_minute_marks = [3, 13, 15, 20, 22, 25, 27]
 #    second_marks.append(m*60)
 
 #import all hr stats
-def splitPPGSignals(base_path, fs, length):
+def splitSignals(base_path, fs, length, file_type):
     endArray = []
     endClassifierArray = []
-    segmentTranslator = fs*length
+    segmentTranslator = fs*length # amount of hz per segment 
     for i in range(1,21):
-        print (base_path)
-        file_path = (f"{base_path}{i}/BVP.csv")
+        file_path = (f"{base_path}{i}/{file_type}")
         signal = pd.read_csv(file_path).squeeze().to_numpy()
 
         # split into 60 second segments
@@ -66,9 +65,6 @@ def splitPPGSignals(base_path, fs, length):
         segmentArray = trimmed_signal.reshape(-1, step)
 
         endArray.append(segmentArray)
-
-        print (len(segmentArray))
-        print (len(segmentArray[3]))
 
         endMinuteMarks = calculateEndMinuteMarks(signal, fs, length)
 
@@ -103,8 +99,13 @@ def main():
     fs = 64
     length = 60 # 60 seconds
     file_path_base = "Dataset/Data_29_subjects/Subjects/subject_"
-    PPGSignalSegmentArray = splitPPGSignals(file_path_base, fs, length)[0] # use this to extract features about each segment
-    PPGsignalsegmentClassifierArray = splitPPGSignals(file_path_base, fs, length)[1] # this is the classifier for stress (1 or 0) for each of the segments
+    PPGSignalSegmentArray = splitSignals(file_path_base, fs, length, "BVP.csv")[0] # use this to extract features about each segment
+    PPGsignalsegmentClassifierArray = splitSignals(file_path_base, fs, length, "BVP.csv")[1] # this is the classifier for stress (1 or 0) for each of the segments
+
+    EDASignalSegmentArray = splitSignals(file_path_base, 4,length, "EDA.csv")[0]
+
+    print (len(PPGSignalSegmentArray[6]))
+    print (len(EDASignalSegmentArray[6]))
 
     #getPPGFeatures(PPGSignalSegmentArray)
 
